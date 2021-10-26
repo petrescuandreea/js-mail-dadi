06e0f4b8dd2e86234d5f9053b2fdad5301f7a7f5
Andreea1696 <andreeapetrescu16@gmail.com>
2021-10-26 16:08:13 +0200

    ciclo for

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,6 +1,7 @@
 /*Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (che hai fatto tu precedentemente), stampa un messaggio appropriato sull’esito del controllo.
 */
 
+const checkMessage =document.getElementById("messagge");
 
 // Creo un array con le mail degli invitati 
 const guestList = ["luca.bianco@gmail.com" , "elisa.filippi@gmail.com" , "maria.magri@yahoo.com" , "alessandro.verdi@gmail.com" , "giovanni.rossi@yahoo.com" , "elisabetta.rizzo@gmail.com"];
@@ -11,10 +12,17 @@ for (let i = 0; i < guestList.length; i++){
 
 // Chiedo all'utente di inserire la sua mail
 let userEmail = prompt("Inserici la tua email");
+console.log("L'email inserita dall'utente è:" , userEmail);
 
 // Controllo se può accedere o meno 
+for (let i = 0; i < guestList.length; i++){
+    // Se la sua mail è nell'array creato allora può entrare 
+    if (userEmail === guestList[i]){
+        checkMessage.innerHTML= ("Ok, sei nella lista degli invitati. Puoi accedere.");
+    }else{
+        // Se la sua mail non è presente nell'array non può entrare 
+        checkMessage.innerHTML= ("Non risulti presente nella lista degli invitati. Mi dispiace, non puoi accedere.");
+    }
+}
 
-// Se la sua mail è nell'array creato allora può entrare 
-
-// Se la sua mail non è presente nell'array non può entrare 
 
